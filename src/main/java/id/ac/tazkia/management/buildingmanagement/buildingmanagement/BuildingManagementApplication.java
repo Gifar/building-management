47f5619832e6eebc07609c3746e221f2a4db3e5b
dimashadiwibowo <dimashadiwibowo13@gmail.com>
2018-11-08 12:26:51 -0500

    screen building management

--- src/main/java/id/ac/tazkia/management/buildingmanagement/buildingmanagement/BuildingManagementApplication.java
+++ src/main/java/id/ac/tazkia/management/buildingmanagement/buildingmanagement/BuildingManagementApplication.java
@@ -2,6 +2,9 @@ package id.ac.tazkia.management.buildingmanagement.buildingmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BuildingManagementApplication {
---
+++ src/main/java/id/ac/tazkia/management/buildingmanagement/buildingmanagement/controller/BookingRuanganController.java
@@ -0,0 +1,11 @@
+package id.ac.tazkia.management.buildingmanagement.buildingmanagement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookingRuanganController {
+    @GetMapping("/bookingruangan/list")
+    public void list(){
+    }
+}
